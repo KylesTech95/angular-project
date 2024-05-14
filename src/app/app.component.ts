5d7c936ffcbe29b2e71bb323889f1a643cfa074b
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-05-14 15:18:11 -0400

    fork: rename app-root element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'custom-root-element',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
